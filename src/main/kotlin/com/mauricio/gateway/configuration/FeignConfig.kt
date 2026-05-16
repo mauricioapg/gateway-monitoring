75edfeba25c2df45c3ad76fdbf656b1687a226a4
mauriciokoruja2 <mauricio@korujatec.com>
2026-05-16 16:30:59 -0300

    mexidas no pom xml

--- src/main/kotlin/com/mauricio/gateway/configuration/FeignConfig.kt
+++ src/main/kotlin/com/mauricio/gateway/configuration/FeignConfig.kt
@@ -2,43 +2,36 @@ package com.mauricio.gateway.configuration
 
 import feign.Logger
 import feign.RequestInterceptor
-import okhttp3.ConnectionPool
+import feign.codec.ErrorDecoder
 import okhttp3.OkHttpClient
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.concurrent.TimeUnit
 
 @Configuration
-class FeignConfig(
-    @Value("\${monitoring-api.security.key}")
-    private val apiKey: String
-) {
+class FeignConfig {
 
     private val log = LoggerFactory.getLogger(FeignConfig::class.java)
 
     @Bean
     fun okhttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS)
-            .connectionPool(ConnectionPool(10, 5, TimeUnit.MINUTES))
-            .retryOnConnectionFailure(true)
+            .followRedirects(true)
+            .followSslRedirects(true)
             .build()
-    }
-
-    @Bean
-    fun feignLoggerLevel(): Logger.Level {
-        return Logger.Level.FULL
     }
 
     @Bean
     fun requestInterceptor(): RequestInterceptor {
         return RequestInterceptor { template ->
 
-            template.header("x-api-key", apiKey)
+            template.header(
+                "x-api-key",
+                "zurgD6SwnioG1bcAHqnjBPdBSWDjw2GuzNQNzgaEMo82HJRseynjyFwJ9tDcXIyG"
+            )
 
             log.info("""
                 FEIGN REQUEST
@@ -47,5 +40,15 @@ class FeignConfig(
                 HEADERS: ${template.headers()}
             """.trimIndent())
         }
+    }
+
+    @Bean
+    fun feignLoggerLevel(): Logger.Level {
+        return Logger.Level.FULL
+    }
+
+    @Bean
+    fun errorDecoder(): ErrorDecoder {
+        return ErrorDecoder.Default()
     }
 }
